81db04bd773b4a7a706667e261df87f39397ba41
vincentferraro <v.ferraro@live.fr>
2023-10-15 12:56:14 -0400

    generate angular project and create component with CLI

---
+++ myApp/src/app/component-test/component-test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-test',
+  templateUrl: './component-test.component.html',
+  styleUrls: ['./component-test.component.css']
+})
+export class ComponentTestComponent {
+
+}
