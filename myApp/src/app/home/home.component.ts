a8d773534e7a624babef0b4f05ea278c62817a47
vincentferraro <v.ferraro@live.fr>
2023-10-15 15:15:08 -0400

    add input in housing-location component

--- myApp/src/app/home/home.component.ts
+++ myApp/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-
+import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports:[HousingLocationComponent],
+  imports:[HousingLocationComponent,CommonModule],
   template: `
   <section>
   <form>
@@ -13,11 +14,23 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   </form>
 </section>
 <section class="results">
-<app-housing-location></app-housing-location>
+<app-housing-location [housingLocation]="housingLocation"></app-housing-location>
 </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  readonly baseUrl='https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+
+  }
 }
